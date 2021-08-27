95d19655d98fc2eb7c1c2792532f5b6d6951854a
ProgrammingInBlood <eklavyasingh12065@gmail.com>
2021-08-27 21:17:40 +0530

    okey

--- src/pages/api/products/image.js
+++ src/pages/api/products/image.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
     form.on('fileBegin', function (name, file) {
       console.log(slugify(file.name));
       file.path = path.join(`public`, slugify(file.name));
-      paths = `${process.env.BASE_URL}-${time}-${slugify(file.name)}`;
+      paths = `${process.env.BASE_URL}/${slugify(file.name)}`;
       arrays.push(paths);
     });
 
